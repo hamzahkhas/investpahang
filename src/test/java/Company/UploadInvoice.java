fc1b32a7c02c3142fc6d3c9a262e259e05557bee
hamzahkhas <s58643@ocean.umt.edu.my>
2024-08-09 11:32:23 +0800

    .

--- src/test/java/Company/UploadInvoice.java
+++ src/test/java/Company/UploadInvoice.java
@@ -16,7 +16,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
