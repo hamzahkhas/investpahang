fc02434533586495fcab1ae7b5771cc212893d6a
hamzahkhas <s58643@ocean.umt.edu.my>
2024-08-09 11:11:53 +0800

    fix sidebar upload invoice

--- src/test/java/Company/UploadInvoice.java
+++ src/test/java/Company/UploadInvoice.java
@@ -1,7 +1,9 @@
 package Company;
 
+import org.junit.jupiter.api.Test;
 import java.io.*;
 import java.time.Duration;
+import java.util.concurrent.TimeoutException;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
@@ -14,19 +16,20 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import dev.failsafe.TimeoutExceededException;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class UploadInvoice {
-    
-    public static void main(String[] args) {
+
+    @Test
+    public static void main(String[] args) throws TimeoutException {
         
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         Actions actions = new Actions(driver);
         JavascriptExecutor js = (JavascriptExecutor) driver;
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 
         driver.manage().window().maximize();
         
@@ -39,47 +42,51 @@ public class UploadInvoice {
         driver.findElement(By.xpath("//*[@id=\"passwordGroup\"]/div/input")).sendKeys("password");
         driver.findElement(By.xpath("/html/body/div[1]/div/div[3]/div/div/form/button")).click();
 
-        // reveal sidebar (on hover etc)
-        WebElement displaySidebar = driver.findElement(By.xpath("//*[@id=\"app\"]/div[2]/div[1]/nav/div[1]/button"));
-        actions.moveToElement(displaySidebar).perform();
-        driver.findElement(By.xpath("//*[@id=\"app\"]/div[2]/div[1]/nav/div[1]/button")).click();
-
-        // view application submenu
-        WebElement investmentMenu = driver.findElement(By.xpath("//*[@id=\"adminmenu\"]/ul/li[3]/a"));
-        actions.moveToElement(investmentMenu).perform();
-        investmentMenu.click();
-        WebElement viewApplication = driver.findElement(By.xpath("//*[@id=\"sidebar-investment-dropdown-list\"]/li[2]/a"));
-        js.executeScript("arguments[0].click()", viewApplication);
+        // open project progress submenu
+        WebElement investMenu = driver.findElement(By.xpath("//*[@id=\"sidebar\"]/div/div[2]/nav/ul/li[3]/a"));
+        actions.moveToElement(investMenu).perform();
+        investMenu.click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"sidebar\"]/div/div[2]/nav/ul/li[3]/div/ul/li[6]/a"))).click();
+       
+        driver.findElement(By.xpath("//*[@id=\"dataTable\"]/thead/tr/th[2]")).click();
+        driver.findElement(By.xpath("//*[@id=\"dataTable\"]/thead/tr/th[2]")).click();
 
         boolean applicationFound = false;
 
         // to find the required applications
         while (!applicationFound) {
             try {
-                WebElement applicationId = driver.findElement(By.xpath("//*[@id=\"298\"]/td[8]/div/a[3]"));
-                applicationFound = true;
-                applicationId.click();
+                // Try to locate the application by its XPath
 
-            } catch (Exception e) {
+                WebElement applicationId = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"339\"]/td[8]/div/a[3]")));
+                applicationFound = true;
+                ((JavascriptExecutor) driver).executeScript("arguments[0].click();", applicationId);
+                System.out.println("Application found and clicked.");
+
+            } catch (org.openqa.selenium.TimeoutException e) {
+
                 try {
+                    // Look for the "Next" button and click it
                     WebElement nextPage = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"dataTable_next\"]/a")));
                     nextPage.click();
-                    wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"298\"]/td[8]/div/a[3]"))); // wait for an element on the next page to ensure the page is loaded
+
+                    // Wait for the new page to load
+                    wait.until(ExpectedConditions.stalenessOf(nextPage)); // Wait for the page to refresh
+                    wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//table[@id='dataTable']"))); // Wait for the table to be visible
 
                 } catch (NoSuchElementException ex) {
-                    System.out.println("Next button not found "+ex);
-                    break;
-                } catch (TimeoutExceededException te) {
-                    System.out.println("timeout wait " +te);
+                    // Break the loop if "Next" button is not found or a timeout occurs
+                    System.out.println("Next button not found or timeout occurred: " + ex);
                     break;
                 }
             }
         }
 
         // upload invoice
-        WebElement uploadProjectImplementationSchedule = driver.findElement(By.cssSelector("#proof_of_payment"));
-        uploadProjectImplementationSchedule.sendKeys("C:\\Users\\hamza\\Desktop\\invest-pahang docs\\Implementation Schedule.pdf"); 
-        driver.findElement(By.xpath("//*[@id=\"app\"]/div[2]/div[2]/div/div[3]/div[2]/div/div/div/div/div[2]/button")).click();
+        Thread.sleep(5000);
+        WebElement uploadInvoice = driver.findElement(By.xpath("//*[@id=\"proof_of_payment\"]"));   // not catered for different payment types yet due to some bullshit
+        uploadInvoice.sendKeys("C:\\Users\\hamza\\Desktop\\invest-pahang docs\\Implementation Schedule.pdf"); 
+        driver.findElement(By.xpath("/html/body/div[2]/div[2]/main/div[2]/div[2]/div[2]/div/div/div/div/div[2]/button")).click();
 
         // switch to confirmation alert
         Alert confirmationAlert = driver.switchTo().alert();
